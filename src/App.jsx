832b0ebdc11a1bdaa040799dd3a975d5f9666cdf
M00N15 <moonis.ahmed@surveyauto.email>
2023-06-09 13:45:15 +0500

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import discordLogo from "./assets/discord_main_logo.svg";
 function App() {
   return (
     <div style={{ backgroundColor: "#5865f2" }}>
-      <img src={discordLogo} alt="discord" />
+      <img style={{margin:"5px 0px 5px 10px", padding:"0px 0px 0px 5px"}} src={discordLogo} alt="discord" />
     </div>
   );
 }
